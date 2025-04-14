fd41562fa80bbb08cf6834fc89eac6e6666d0e8d
Jaydeep Gohil <167198695+Jaydeep-Gohil@users.noreply.github.com>
2025-04-14 11:44:22 +0530

    v1.1.0
    create a Custome Widget.

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -41,7 +41,85 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("Flutter"),
       ),
-      body: ElevatedButton(child: Text("Login"),onPressed: callBack,),
+      body: Column(
+        children: [ProfileIcon(),ProfileName(),Footer()],
+      )
       );
   }
+}
+
+class ProfileIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        height: 100,
+        color: Colors.blueAccent,
+        child: ListView.builder(scrollDirection: Axis.horizontal,
+          itemBuilder: (context,
+              index)=>Padding(padding: EdgeInsets.all(10),
+            child: SizedBox(
+              width: 100,
+              child: CircleAvatar(
+                backgroundColor: Colors.green,
+              ),
+            ),),
+          itemCount: 10,
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileName extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  return Expanded(
+    flex: 3,
+    child: Container(
+        height: 100,
+        color: Colors.greenAccent,
+        child: ListView.builder(
+          itemBuilder: (context,index)=>
+              Padding(padding: EdgeInsets.all(11),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.green,
+                    ),
+                    title:Text("Name "),
+                    subtitle: Text("Mobile no "),
+                    trailing: Icon(Icons.delete),
+                  )
+              ),
+        )
+
+    ),
+  );
+  }
+
+}
+
+class Footer extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return Expanded(
+     flex: 2,
+     child: Container(
+       height: 100,
+       color: Colors.lightGreenAccent,
+       child: ListView.builder(itemBuilder: (context,index)=>Padding(padding: EdgeInsets.all(10),
+         child: Container(
+           width: 200,
+           decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(10),
+               color: Colors.blueAccent
+           ),
+         ),
+       ), itemCount : 10,scrollDirection: Axis.horizontal,
+       ),
+     ),
+   );
+  }
+
 }
